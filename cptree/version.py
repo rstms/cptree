989ff79b659be2103e9b48c6945a747230b7f6b5
Matt Krueger <mkrueger@rstms.net>
2024-02-11 08:08:56 -0700

    Bump version: 0.1.7 → 0.2.0

--- cptree/version.py
+++ cptree/version.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.7"
+__version__ = "0.2.0"
 __author__ = "Matt Krueger"
 __email__ = "mkrueger@rstms.net"
-__timestamp__ = "2024-02-06 08:39:48-07:00"
+__timestamp__ = "2024-02-11 08:08:56-07:00"
